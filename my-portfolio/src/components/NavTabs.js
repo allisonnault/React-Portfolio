d10268171dcf8542d688ea7886d0d59b99dec086
allisonnault <allisonrnault@gmail.com>
2023-03-27 19:13:30 -0500

    worked on navbar

--- my-portfolio/src/components/NavTabs.js
+++ my-portfolio/src/components/NavTabs.js
@@ -2,46 +2,52 @@ import React from 'react';
 
 function NavTabs({ currentPage, handlePageChange }) {
     return (
-      <ul className="nav nav-tabs">
-        <li className="nav-item">
-          <a
-            href="#aboutMe"
-            onClick={() => handlePageChange('AboutMe')}
-            className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}
-          >
-            About Me
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            href="#portfolio"
-            onClick={() => handlePageChange('Portfolio')}
-            className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-          >
-            Portfolio
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            href="#contact"
-            onClick={() => handlePageChange('Contact')}
-            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-          >
-            Contact
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            href="#resume"
-            onClick={() => handlePageChange('Resume')}
-            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-          >
-            Resume
-          </a>
-        </li>
-      </ul>
+        <div className='row container-fluid align-items-center p-3'>
+            <div className='col-5'>
+                <h1>Allison Nault</h1>
+            </div>
+            <div className='col'>
+                <ul className="nav justify-content-end">
+                    <li className="nav-item">
+                        <a
+                            href="#aboutMe"
+                            onClick={() => handlePageChange('AboutMe')}
+                            className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}
+                        >
+                            About Me
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a
+                            href="#portfolio"
+                            onClick={() => handlePageChange('Portfolio')}
+                            className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+                        >
+                            Portfolio
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a
+                            href="#contact"
+                            onClick={() => handlePageChange('Contact')}
+                            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                        >
+                            Contact
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a
+                            href="#resume"
+                            onClick={() => handlePageChange('Resume')}
+                            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                        >
+                            Resume
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     );
-  }
-  
-  export default NavTabs;
-  
+}
+
+export default NavTabs;
