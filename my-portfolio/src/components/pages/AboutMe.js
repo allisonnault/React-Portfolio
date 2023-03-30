b065dc01856739ab5c6d6ec4edc25adef0f40231
allisonnault <allisonrnault@gmail.com>
2023-03-29 22:15:52 -0500

    added css

--- my-portfolio/src/components/pages/AboutMe.js
+++ my-portfolio/src/components/pages/AboutMe.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import image from '../../images/myPic.jpeg'; 
 
 export default function AboutMe() {
-    return (
-    <div className='p-3'>
-      <h3>About Me</h3>
-      <p>
-      A highly motivated team leader with 2 years of professional sales experience in the fitness industry. A top performer able to leverage Hubspot CRM tools to increase productivity, ensure quality standards, and implement best practices. Experienced in overseeing multiple departments, training staff, and developing team members
-      </p>
+  return (
+    <div className='p-3 row'>
+      <div className='col-6'>
+        <div className='card'>
+        <img src={image}></img>
+        </div>
+        
+      </div>
+      <div className='col-6'>
       <div>
+      <p>
+          A highly motivated team leader with 2 years of professional sales experience in the fitness industry. A top performer able to leverage Hubspot CRM tools to increase productivity, ensure quality standards, and implement best practices. Experienced in overseeing multiple departments, training staff, and developing team members
+        </p>
         <h3>Technical Skills</h3>
         <p>
-        JavaScript, CSS, HTML, SQL, NoSQL, GitHub, MongoDB, MySQL, Express, Node, Handlebars, JQuery, Bootstrap, React
+          JavaScript, CSS, HTML, SQL, NoSQL, GitHub, MongoDB, MySQL, Express, Node, Handlebars, JQuery, Bootstrap, React
         </p>
       </div>
       <div>
@@ -29,7 +36,8 @@ export default function AboutMe() {
             Employee training and development
           </li>
         </ul>
-        </div>
+      </div>
+      </div>
     </div>
-    )
+  )
 }
--- my-portfolio/src/components/pages/Portfolio.js
+++ my-portfolio/src/components/pages/Portfolio.js
@@ -11,20 +11,24 @@ const globe = <FontAwesomeIcon icon={faGlobe} size="lg" />
 export default function Portfolio() {
   return (
     <div className='p-3 flex-row justify-center'>
-      <h3>Portfolio</h3>
+      <h3 className='text-center'>Portfolio</h3>
       <div className="row flex-row justify-space-between my-4">
-      {portfolioData.map((project) => (
-        <div key={project.id} className="col-12 col-xl-6 ">
-          <div className='m-2 mb-4 shadow project-card'>
-            <img src={project.image} className="card-img-top"></img>
-            <div className='card-body'>
-            <h5 className='card-title p-2 m-0'>{project.title}</h5>
-            <a href={project.gitHub} className="btn" target="_blank">{gitHub}</a>
-            <a href={project.deployed} className="btn" target="_blank">{globe}</a>
+        {portfolioData.map((project) => (
+          <div key={project.id} className="col-12 col-xl-6 ">
+            <div className='m-2 mb-4 shadow project-card'>
+              <img src={project.image} className="card-img-top project-image"></img>
+              <div className='row card-body'>
+                <div className='col-6'>
+                  <h5 className='card-title p-2 m-0'>{project.title}</h5>
+                </div>
+                <div className='col-6 text-end'>
+                  <a href={project.gitHub} className="btn" target="_blank">GitHub {gitHub}</a>
+                  <a href={project.deployed} className="btn" target="_blank">Web App {globe}</a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
       </div>
     </div>
   );
