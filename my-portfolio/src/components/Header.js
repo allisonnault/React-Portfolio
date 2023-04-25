4008f08740334a51560d2c4e16ca2dbb27587045
allisonnault <allisonrnault@gmail.com>
2023-04-25 16:02:11 -0500

    removed contact page

--- my-portfolio/src/components/Header.js
+++ my-portfolio/src/components/Header.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Nav from './Nav';
 import AboutMe from './pages/AboutMe';
 import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
 import Resume from './pages/Resume';
 
 export default function PortfolioContainer() {
@@ -14,9 +13,6 @@ export default function PortfolioContainer() {
         }
         if (currentPage === 'Portfolio') {
             return <Portfolio />;
-        }
-        if (currentPage === 'Contact') {
-            return <Contact />;
         }
         return <Resume />;
     };
--- my-portfolio/src/components/Nav.js
+++ my-portfolio/src/components/Nav.js
@@ -28,15 +28,6 @@ function Nav({ currentPage, handlePageChange }) {
                     </li>
                     <li className="nav-item">
                         <a
-                            href="#contact"
-                            onClick={() => handlePageChange('Contact')}
-                            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-                        >
-                            Contact
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a
                             href="#resume"
                             onClick={() => handlePageChange('Resume')}
                             className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
